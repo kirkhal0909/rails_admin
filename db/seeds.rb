95d5ebcc15a1603d8651722ce3e6dbd0144531ba
Kirill Khalevin <kirkhal0909@gmail.com>
2023-05-22 18:18:09 +0300

    docker-compose (#4)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,30 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Admin.create(email: 'admin@admin.ru', password: 'admin@admin.ru', password_confirmation: 'admin@admin.ru')
+def init_objects
+  Admin.create(email: 'admin@admin.ru', password: 'admin@admin.ru', password_confirmation: 'admin@admin.ru')
 
-4.times do |user_index|
-  User.create(
-    name: "user#{user_index}",
-    email: "user#{user_index}@user.ru",
-    password: "user#{user_index}@user.ru",
-    password_confirmation: "user#{user_index}@user.ru"
-  )
-end
+  4.times do |user_index|
+    User.create(
+      name: "user#{user_index}",
+      email: "user#{user_index}@user.ru",
+      password: "user#{user_index}@user.ru",
+      password_confirmation: "user#{user_index}@user.ru"
+    )
+  end
 
-users = User.all
+  users = User.all
 
-Post.create(title: 'Some topic', content: 'some content', user: users[0])
-Post.create(title: 'Some topic 2', content: 'some content 2', user: users[1])
-Post.create(title: 'Some topic 3', content: 'some content 3', user: users[1])
+  Post.create(title: 'Some topic', content: 'some content', user: users[0])
+  Post.create(title: 'Some topic 2', content: 'some content 2', user: users[1])
+  Post.create(title: 'Some topic 3', content: 'some content 3', user: users[1])
 
-Post.all.each do |post|
-  rand(1..4).times do |i|
-    post.likes.create(user: users[i], post: post)
-    post.comments.create(user: users[i], post: post, content: "content message #{i}")
+  Post.all.each do |post|
+    rand(1..4).times do |i|
+      post.likes.create(user: users[i], post: post)
+      post.comments.create(user: users[i], post: post, content: "content message #{i}")
+    end
   end
 end
+
+init_objects unless Admin.last
